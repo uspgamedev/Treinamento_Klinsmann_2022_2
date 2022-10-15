377edef15ec0116899e2708bd0019f70da694daf
MarceloSpessoto <marcelomspessoto@gmail.com>
2022-10-15 12:35:26 -0300

    Protótipo de fase 1
    Signed-off-by: MarceloSpessoto <marcelomspessoto@gmail.com>

--- Assets/Examples/ExampleScripts/ExampleWaterEnemy.cs
+++ Assets/Examples/ExampleScripts/ExampleWaterEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterEnemy : Enemy
+public class ExampleWaterEnemy : Enemy
 {
     public float ymoveSpeed;
     void Start()
