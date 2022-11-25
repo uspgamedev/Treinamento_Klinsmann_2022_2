22fa050e76b8404af6997fb60eff0e82977f9140
cecyayumi <113843997+cecyayumi@users.noreply.github.com>
2022-11-25 19:10:09 -0300

    Mudanças bg, sprites e ui

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -12,13 +12,11 @@ public class DialogueManager : MonoBehaviour
     public TMP_Text dialogueText;
     public Animator animator;
     private Queue<string> sentences;
-
+  
     // Start is called before the first frame update
     void Start()
     {
-
         sentences = new Queue<string>();
-        
     }
 
     public void StartDialogue(Dialogue dialogue)
@@ -74,7 +72,7 @@ public class DialogueManager : MonoBehaviour
         //}
         void EndDialogue()
         {
-            animator.SetBool("IsOpen", false); ;
+            animator.SetBool("IsOpen", false); 
         }
 
     }
